0b5608a2728ea089faf11873d73909a9437f4e90
Idode Kerobo <idodekerobo@Idodes-MacBook-Pro.local>
2019-12-31 16:38:09 -0700

    added nodemailer n changed header to name

--- index.js
+++ index.js
@@ -19,8 +19,13 @@ const PORT = process.env.PORT || 5000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// USE BODYPARSER
+app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+
 // SERVING STATIC FILES
 app.use('/public',express.static(path.join(__dirname,'/public')));
+
 
 // =====================================================
 // APP ROUTES
@@ -31,7 +36,46 @@ app.get('/', function(req, res) {
 });
 
 app.post('/signup', function(req, res) {
-    res.send('thanks for reaching out');
+    res.send('Thanks for reaching out!');
+
+    const output = "<p>Someone reached out from your website contact form!</p>" + 
+    "<li>" + req.body.name + "</li>" + 
+    "<li>" + req.body.email + "</li>" + 
+    "<li>" + req.body.description + "</li>";
+
+    var transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com', 
+        port: 465,
+        secure: true,
+        auth: {
+            type: 'OAuth2',
+            user: 'idode.kerobo@gmail.com',
+            clientId: '27597703654-mg8ksk4lra76c59i8stlis86ogkf099o.apps.googleusercontent.com',
+            clientSecret: 'OPvyto2lehZTOg02-vHb52MW',
+            refreshToken: '1//0455wmwRUukqzCgYIARAAGAQSNwF-L9IrsNyETScG2XtwYGc9gcvrphmSSR_mQCO9OzdmJCk4Td4z5-tOeemKNN9KIITESztUQ38',
+            accessToken: 'ya29.Il-3B0xM5S56xAGsLYx77dBKAIWXEx6s-kCf02uqgMutGVgybRE6q4EBU9RtANJotx6Ysxi3mcaxB8d21-Dup_F6g2-IGHTrXzMGs_zHgIvJLbswJ9CEqv3zgoROLn0uTA'
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    var mailOptions = {
+        from: req.body.email,
+        to: 'idode.kerobo@gmail.com',
+        subject: req.body.name + ' reached out via Personal Site Contact Form',
+        text: 'plain text',
+        html: output
+    };
+
+
+    transporter.sendMail(mailOptions, function(err, res) {
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log(JSON.stringify(res));
+        }
+    });
 });
 
 // =====================================================
