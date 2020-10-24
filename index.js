726fd5c353482982da45ea755279246d34ed9cfb
Idode Kerobo <idode.kerobo@gmail.com>
2020-10-24 16:46:05 -0700

    commenting out nodemailer

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 
 // =====================================================
 // APP VARIABLES
